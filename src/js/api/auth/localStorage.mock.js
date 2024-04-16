228a938e2fd6fca6033d14cad2e79a0e0047b3da
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-04-16 09:53:59 +0200

    added some notes to recall later

--- src/js/api/auth/localStorage.mock.js
+++ src/js/api/auth/localStorage.mock.js
@@ -1,3 +1,8 @@
+/*exports an object containing mocked implementations of
+ the setItem, getItem, and removeItem methods.The export
+  default syntax is used to export this object as the default
+   export of the module */
+
 export default {
   setItem: jest.fn((key, value) => (localStorage[key] = value)),
   getItem: jest.fn((key) => localStorage[key] || null),
