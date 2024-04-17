c806e611f487ea4c2d868db5ba52918692021b5e
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-04-17 10:40:15 +0200

    cypress test for failed log in

--- src/js/api/auth/localStorage.mock.js
+++ src/js/api/auth/localStorage.mock.js
@@ -1,10 +1,22 @@
-/*exports an object containing mocked implementations of
- the setItem, getItem, and removeItem methods.The export
-  default syntax is used to export this object as the default
-   export of the module */
+/*each function (setItem, getItem, and removeItem)
+is defined separately using jest.fn(), and then
+they are assigned to the corresponding properties
+of the object being exported */
+
+const setItem = jest.fn((key, value) => {
+  localStorage[key] = value;
+});
+
+const getItem = jest.fn((key) => {
+  return localStorage[key] || null;
+});
+
+const removeItem = jest.fn((key) => {
+  delete localStorage[key];
+});
 
 export default {
-  setItem: jest.fn((key, value) => (localStorage[key] = value)),
-  getItem: jest.fn((key) => localStorage[key] || null),
-  removeItem: jest.fn((key) => delete localStorage[key]),
+  setItem,
+  getItem,
+  removeItem,
 };
